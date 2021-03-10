44c3415ad9580199ba2234035c69cb1ff5b75816
nimoc <>
2021-03-10 22:46:46 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/computer_systems_note.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/computer_systems_note.dir/main.c.o"
+  "computer_systems_note"
+  "computer_systems_note.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/computer_systems_note.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
